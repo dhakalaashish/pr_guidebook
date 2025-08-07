955d326bdc89f6bc2d68e3a528e301f4dbe2fce1
Aashish Dhakal <85501584+dhakalaashish@users.noreply.github.com>
2025-08-07 11:28:03 -0400

    Updated react to use react router

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss()
-  ],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000'
-    }
-  }
-})
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+});
